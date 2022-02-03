fa2382eeda04f81e6850fd7e597386aa0292db72
vladimir-asus <vladimir@vladimir-e200ha>
2022-02-03 20:47:35 +0500

    Show most popular posts on main page

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
-admin.site.register(Tag)
-admin.site.register(Comment)
+# admin.site.register(Tag)
+# admin.site.register(Comment)
 # admin.site.register(Post)
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    raw_id_fields = ('likes', 'author', 'tags')
+    raw_id_fields = ('likes', 'author', 'tags',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    raw_id_fields = ('post', 'author',)
