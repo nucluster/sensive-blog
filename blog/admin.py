5c76d3e9ace1b42cbeaad82f7d833bd5188de38a
vladimir-asus <vladimir@vladimir-e200ha>
2022-02-03 20:08:01 +0500

    Add PostAdmin with raw_id_fields

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
-admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Comment)
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    raw_id_fields = ('likes', 'author', 'tags')
